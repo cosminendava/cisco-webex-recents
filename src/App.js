1ab4a9fad56b666f2293f5e7a8d9d05d95a413d1
Cosmin Cristian Onofre <Cosmin.Onofre@endava.com>
2020-12-16 12:56:50 +0200

    Recents Widget - LCC communication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-// import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import RecentsWidget from '@webex/widget-recents';
 import '@webex/widget-recents/src/momentum.scss';
+import qs from 'querystring';
+import LCC from 'lightning-container';
 
-function App() {
-  return (
-    <div className="App">
-      <RecentsWidget accessToken='ZDhmN2ZlNzEtYWRiNC00ZWViLTllYjEtN2FjZTdkZDAyZTlhNDQ4MDFiZjctZmM0_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f' />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const searchString = this.props.location.search.replace('?', '');
+    const searchParams = qs.parse(searchString);
+    this.state = {
+      accessToken: searchParams.accessToken
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <RecentsWidget accessToken={this.state.accessToken} onEvent={this.callback}/>
+      </div>
+    );
+  }
+
+  callback(name, detail) {
+    console.log('React', name);
+    console.log('React', detail);
+
+    LCC.sendMessage({
+      name,
+      detail
+    });
+  }
 }
 
 export default App;
